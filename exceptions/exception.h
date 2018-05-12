bb9ea8da912d5353d4a3999e0dedb2e4b690e6e8
Iskander <iiskander05@gmail.com>
2018-05-12 19:19:38 +0300

    code style changes

--- exceptions/exception.h
+++ exceptions/exception.h
@@ -4,7 +4,6 @@
 #include <setjmp.h>
 #include <iostream>
 
-
 class Object {
 public:
 	Object();
@@ -17,7 +16,6 @@ public:
 	virtual ~Exception() {};
 };
 
-
 std::vector<jmp_buf*> environments;
 jmp_buf* environment = nullptr;
 std::vector<int> shifts;
@@ -25,42 +23,39 @@ std::vector<Object*> objects;
 Exception* exception = nullptr;
 int returnValue = 0;
 
-
-Object::Object() {
-	objects.push_back(this);
-}
+Object::Object() { objects.push_back(this); }
 
 Object::~Object() {
 	std::cout << "Calling local object destruction" << std::endl;
 	objects.pop_back();
 }
 
+#define TRY                           \
+  shifts.push_back(objects.size());   \
+  environment = new jmp_buf[1];       \
+  returnValue = setjmp(*environment); \
+  if (returnValue == 0) {             \
+    environments.push_back(environment);
 
-#define TRY shifts.push_back(objects.size());												\
-		    environment = new jmp_buf[1];													\
-			returnValue = setjmp(*environment);												\
-			if (returnValue == 0) {															\
-				environments.push_back(environment);										\
+#define CATCH(type, error_ptr)                      \
+  }                                                 \
+  delete environments.back();                       \
+  environments.pop_back();                          \
+  type* error_ptr = dynamic_cast<type*>(exception); \
+  if (error_ptr == nullptr) {                       \
+    THROW(exception);                               \
+  } else {                                          \
+    exception = nullptr;                            \
+  }
 
-
-#define CATCH(type, error_ptr) }															\
-						delete environments.back();											\
-						environments.pop_back();											\
-						type* error_ptr = dynamic_cast<type*>(exception);					\
-						if (error_ptr == nullptr) {											\
-							THROW(exception);												\
-						} else {															\
-						    exception = nullptr;											\
-						}
-
-
-#define THROW(e) if (e == nullptr || environments.size() == 0) {							\
-				     exit(1);																\
-				 } else {																	\
-				     for (int i = objects.size() - 1; i >= shifts.back(); --i) {			\
-					     objects[i]->~Object();												\
-					 }																		\
-					 shifts.pop_back();														\
-					 exception = e;															\
-                     longjmp(*environments.back(), 1);										\
-				 }
+#define THROW(e)                                                \
+  if (e == nullptr || environments.size() == 0) {               \
+    exit(1);                                                    \
+  } else {                                                      \
+    for (int i = objects.size() - 1; i >= shifts.back(); --i) { \
+      objects[i]->~Object();                                    \
+    }                                                           \
+    shifts.pop_back();                                          \
+    exception = e;                                              \
+    longjmp(*environments.back(), 1);                           \
+  }
